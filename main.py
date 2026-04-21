db5e9f0883710067f27e1a7ecaaed730e03e6621
Tinogr3 <tinogaarcia04@gmail.com>
2026-04-21 23:29:22 +0200

    bigger screen for brawser

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def main():
     
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
-        context = browser.new_context(viewport={'width': 1280, 'height': 800})
+        context = browser.new_context(viewport={'width': 1440, 'height': 810})
         page = context.new_page()
         page.on("request", intercept_request)
         page.goto("https://www.renfe.com/es/es")
